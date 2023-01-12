05405e698320de5ff63d6f4245e91f1b45a32050
AmanWorku <amexworku2014@gmail.com>
2023-01-12 16:41:44 +0300

    Add style to the page

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react'
-import TodoForm from './TodoForm'
-import TodoList from './TodoList'
+import TodoForm from "./TodoForm";
 import {RiCloseCircleLine} from 'react-icons/ri'
 import {TiEdit} from 'react-icons/ti'
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,23 +6,23 @@ function TodoList() {
 
     const [todos, setTodos] = useState([]);
 
-    const addTodo = todo => {
-        if(!todo.text || /^\s*$/.test(todo.text)){
-            return
-        }
+    const addTodo = (todo) => {
+			if (!todo.text || /^\s*$/.test(todo.text)) {
+				return;
+			}
+			const newTodos = [todo, ...todos];
+			setTodos(newTodos);
+			console.log(...todos);
+		};
 
-        const newTodos = [todo, ...todos];
-        setTodos(newTodos);
-        console.log(...todos);
-    };
-
-    const updateTodo = (todoId, newValue) => {
-        if(!newValue.text || /^\s*$/.test(newValue.text)){
-            return
-        }
-
-        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
-    }
+		const updateTodo = (todoId, newValue) => {
+			if (!newValue.text || /^\s*$/.test(newValue.text)) {
+				return;
+			}
+			setTodos((prev) =>
+				prev.map((item) => (item.id === todoId ? newValue : item))
+			);
+		};
 
     const removeTodo = id => {
         const removeArr = [...todos].filter(todo => todo.id !== id);
